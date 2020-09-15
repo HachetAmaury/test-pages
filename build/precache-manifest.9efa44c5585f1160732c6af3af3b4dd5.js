1ebf0cfdd7a7d0cba05df5cb993636ee0f260fd3
Amaury Hachet <hachet.amaury@gmail.com>
2020-09-15 13:26:27 +0200

    add build

---
+++ build/precache-manifest.9efa44c5585f1160732c6af3af3b4dd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d1f40be69b5ffe73428d8565ccb5e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d134f68c33d6ea66ec70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3deb36ea35ce28b0578",
+    "url": "/static/js/2.efea42fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efea42fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d134f68c33d6ea66ec70",
+    "url": "/static/js/main.2f4238c7.chunk.js"
+  },
+  {
+    "revision": "1550bc5cb6224d84350f",
+    "url": "/static/js/runtime-main.74ac9606.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
